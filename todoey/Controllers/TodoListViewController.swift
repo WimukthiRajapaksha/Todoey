045941cfa27317d99766e90b1e8786b3b10184e6
WimukthiRaj <wimukthirajapaksha.wr@gmail.com>
2018-01-09 19:51:01 +0530

    convert to MVC design pattern.

--- todoey/Controllers/TodoListViewController.swift
+++ todoey/Controllers/TodoListViewController.swift
@@ -10,12 +10,45 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["aaaa","bbbb","cccc","dddd"]
+    var itemArray = [Item]()
     let defaults=UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items=defaults.array(forKey: "TodoListArray") as? [String]{
+        
+        let newItem1=Item()
+        newItem1.title="aaaa"
+//        newItem1.done=true
+        itemArray.append(newItem1)
+        
+        let newItem2=Item()
+        newItem2.title="bbbb"
+        itemArray.append(newItem2)
+        
+        let newItem3=Item()
+        newItem3.title="cccc"
+        itemArray.append(newItem3)
+        
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+//        itemArray.append(newItem3)
+
+        
+        
+        if let items=defaults.array(forKey: "TodoListArray") as? [Item]{
             itemArray=items
         }
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,13 +63,23 @@ class TodoListViewController: UITableViewController {
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text=itemArray[indexPath.row]
+        let item=itemArray[indexPath.row]
+        cell.textLabel?.text=item.title
+        cell.accessoryType=item.done ? .checkmark : .none
         return cell
         
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //        print(itemArray[indexPath.row])
 //        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+//        if itemArray[indexPath.row].done==false{
+//            itemArray[indexPath.row].done=true
+//        }else{
+//            itemArray[indexPath.row].done=false
+//        }
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }else{
@@ -52,12 +95,15 @@ class TodoListViewController: UITableViewController {
         
         let alert=UIAlertController(title: "add new todoe item", message: "aaaa", preferredStyle: .alert)
         let action=UIAlertAction(title: "add item", style: .default) { (action) in
-            print(textField.text!)
+            
+            let newItem=Item()
+            newItem.title=textField.text!
+            self.itemArray.append(newItem)
+//            print(textField.text!)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
-            self.itemArray.append(textField.text!)
-            
+//            self.itemArray.append(textField.text!)
             
             self.tableView.reloadData()
         }
---
+++ todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  todoey
+//
+//  Created by Wimukthi Rajapaksha on 1/9/18.
+//  Copyright © 2018 Wimu. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title:String=""
+    var done:Bool=false
+}
